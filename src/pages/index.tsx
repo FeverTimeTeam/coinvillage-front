195b0a72565dd92deff796568029b69cc9000055
manyyeon <udayeon@naver.com>
2022-09-02 21:50:23 +0900

    feat : 예시로 about 페이지에 styled-components 적용한 버튼 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import Main from './Main/main';
 
 const Home: NextPage = () => {
-  return <div>Welcome to CoinVillage</div>;
+  return (
+    <>
+      <div>Welcome to CoinVillage</div>
+      <Main />
+    </>
+  );
 };
 
 export default Home;
