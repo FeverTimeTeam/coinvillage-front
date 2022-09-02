a916a41ac0e4bb9facb59c84be0506f56a39d6cf
manyyeon <udayeon@naver.com>
2022-09-03 01:22:57 +0900

    docs : color 정리

---
+++ src/constants/color.ts
@@ -0,0 +1,19 @@
+const color = {
+  /* kb 색상 */
+  kb: '#FFBB00',
+  gray: '#BEBEBE',
+  light_gray: '#E2E2E2',
+  light_gray2: '#D1D1D1',
+  deep_green: '#245630',
+  deep: '#3A3A3A',
+  warm_gray_deep: '#544F4C',
+  light_warm_gray: '#D6D3D2',
+  warm_gray1: '#747271',
+  system_information: '#A4A1A0',
+  system_warning: '#FF5242',
+  system_ok: '#3B9A50',
+  white: '#FFFFFF',
+  inactive: '#E3E3E3',
+};
+
+export default color;
