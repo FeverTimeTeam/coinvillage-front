93158082aee913f9a4b5ec47a9e91d7ee275e873
manyyeon <udayeon@naver.com>
2022-09-03 02:13:02 +0900

    refactor : delete Main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next';
-import Main from './Main/main';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>Welcome to CoinVillage</div>
-      <Main />
     </>
   );
 };
