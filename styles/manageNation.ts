b9a2e61996c2aa17ea9a6df0f99f60e5e04f09ae
manyyeon <udayeon@naver.com>
2022-09-04 14:44:03 +0900

    feat : manageNationPage style 수정

--- styles/manageNation.ts
+++ styles/manageNation.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import color from '../src/constants/color';
 
-const Root = styled.div``;
+const Root = styled.div`
+  height: calc(100vh - 5.25rem);
+`;
 
 const TopBarContainer = styled.div`
   display: flex;
@@ -12,4 +14,22 @@ const TopBarLeftItemsContainer = styled.div`
   display: flex;
 `;
 
-export { Root, TopBarContainer, TopBarLeftItemsContainer };
+const ListTitleContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const ListItemContainer = styled.div`
+  display: flex;
+  margin-top: 1.5rem;
+`;
+
+export {
+  Root,
+  TopBarContainer,
+  TopBarLeftItemsContainer,
+  ListTitleContainer,
+  ListItemContainer,
+};
