48611c42a22fc4e25e0be909d2995a01a5d32523
JZU0 <ljzh1030@gmail.com>
2022-09-05 14:36:14 +0900

    feat : passbook manage pc page

---
+++ styles/managePassbook.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import color from '../src/constants/color';
+
+const Root = styled.div`
+  height: calc(100vh - 5.25rem);
+`;
+
+const TopBarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: 7%;
+`;
+
+const ListContentContainer = styled.div`
+  display: flex;
+  margin-top: 2.5rem;
+`;
+const ListDayContentContainer = styled.div`
+  display: flex;
+`;
+
+const GuideBox = styled.div`
+  margin-left: 10%;
+  width: 77%;
+  margin-top: 1.5rem;
+  background-color: ${color.light_gray3};
+  height: 2.5rem;
+  border-radius: 0.8rem;
+  padding: 2.5rem 3%;
+`;
+
+export {
+  Root,
+  TopBarContainer,
+  ListContentContainer,
+  ListDayContentContainer,
+  GuideBox,
+};
