7ec0c1e711f1148a8fe6c0cbfaf5c109c871656f
Seungwoo Lee <timosean@naver.com>
2022-09-09 16:13:59 +0900

    feat: add main_world.svg and edit index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from 'next';
 import Image from '../components/image';
+import styled from 'styled-components';
+
+const IndexPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 5.25rem);
+`;
 
 const Home: NextPage = () => {
   return (
-    <Image
-      src='/main_world_with_background.png'
-      style={{ width: '100%', height: '100%' }}
-    />
+    <IndexPageWrapper>
+      <Image src='/main_world.svg' style={{ width: '85%', height: '80%' }} />
+    </IndexPageWrapper>
   );
 };
 
